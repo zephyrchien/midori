691df131f507f5d95ad2de9d85f70ea78e3e8e52
zephyr <i@zephyr.moe>
2021-07-17 04:46:00 +0900

    set default values for config

--- src/config/ep.rs
+++ src/config/ep.rs
@@ -7,8 +7,11 @@ use super::trans::TransportConfig;
 #[derive(Debug, Serialize, Deserialize)]
 pub struct EpHalfConfig {
     pub addr: String,
+    #[serde(default = "def_net")]
     pub net: NetConfig,
+    #[serde(default = "def_trans")]
     pub trans: TransportConfig,
+    #[serde(default = "def_tls")]
     pub tls: TLSConfig,
 }
 
@@ -17,3 +20,10 @@ pub struct EndpointConfig {
     pub listen: EpHalfConfig,
     pub remote: EpHalfConfig,
 }
+
+// create default values
+fn def_net() -> NetConfig { NetConfig::TCP }
+
+fn def_trans() -> TransportConfig { TransportConfig::Plain }
+
+fn def_tls() -> TLSConfig { TLSConfig::None }
